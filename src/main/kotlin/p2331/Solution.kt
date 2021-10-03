e0484a0dc625f93329f27e96e243aa62f73f40f5
Damotorie <parksjin01@gmail.com>
2021-10-03 14:47:21 +0900

    [BAEKJOON-2331] 반복수열 답안(34005692) 문서 추가

--- src/main/kotlin/p2331/Solution.kt
+++ src/main/kotlin/p2331/Solution.kt
@@ -1,2 +1,38 @@
 package p2331
 
+import kotlin.math.pow
+
+object Solution {
+    fun next(number: Long, factor: Int): Long {
+        var num: Long = 0
+        var tempNumber: Long = number
+
+        while (tempNumber > 0) {
+            num += (tempNumber % 10).toDouble().pow(factor).toLong()
+            tempNumber = tempNumber / 10
+        }
+
+        return num
+    }
+
+    fun solve() {
+        val (a: Int, p: Int) = readLine() ?. split(" ") ?. map { it.toInt() } ?: listOf(0, 0)
+        val numbers: MutableList<Long> = mutableListOf(a.toLong())
+
+        while (true) {
+            val nextNumber: Long = next(numbers.last(), p)
+
+            if (numbers.contains(nextNumber)) {
+                val idx: Int = numbers.indexOf(nextNumber)
+                println(idx)
+                break
+            }
+
+            numbers.add(nextNumber)
+        }
+    }
+}
+
+fun main() {
+    Solution.solve()
+}
